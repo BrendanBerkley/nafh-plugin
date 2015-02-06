3486eabda25994a03cd7484798610b1752536009
Brendan <me@brendanberkley.com>
2015-02-05 22:15:28 -0500

    Work out button link states

--- nafh-plugin.php
+++ nafh-plugin.php
@@ -14,7 +14,7 @@ class Signup_Widget extends WP_Widget {
 	}
 function form($instance) {
 		// outputs the options form on admin
-		$title = $subtitle = $desc = $page_to_link = $link_text = "";
+		$title = $subtitle = $desc = $page_to_link = $button_text = "";
 		if (isset($instance['title'])) :
 			$title = esc_attr($instance['title']);
 		endif;
@@ -27,8 +27,8 @@ function form($instance) {
 		if (isset($instance['page_to_link'])) :
 			$page_to_link = esc_attr($instance['page_to_link']);
 		endif;
-		if (isset($instance['link_text'])) :
-			$link_text = esc_attr($instance['link_text']);
+		if (isset($instance['button_text'])) :
+			$button_text = esc_attr($instance['button_text']);
 		endif;
 
 		echo '<p><label for="' . $this->get_field_id('title') . '">Title:</label> <input class="widefat" id="' . $this->get_field_id('title') . '" name="'. $this->get_field_name('title') .'" type="text" value="'. $title. '" /></p>';
@@ -42,9 +42,9 @@ function form($instance) {
 		echo '<option value="-1">No Link</option>';
 
 		// Generate a dropdown filled with pages in the site to link to.
-		$tgt_pages_array = get_pages(); // Get all the pages
+		$site_pages_array = get_pages(); // Get all the pages
 
-		foreach ($tgt_pages_array as $object) : // Loop through each page object
+		foreach ($site_pages_array as $object) : // Loop through each page object
 			$post_title = ""; // Reset variables
 			$post_id    = "";
 			$selected_text = "";
@@ -72,9 +72,21 @@ function form($instance) {
 
 		echo '</select></p>';
 
-		echo '<p><label for="' . $this->get_field_id('link_text') . '">Link Text:</label> <input class="widefat" id="' . $this->get_field_id('link_text') . '" name="'. $this->get_field_name('link_text') .'" type="link_text" value="'. $link_text. '" /></p>';
 
-		echo '<p>All fields are optional. The widget will figure it out. Play with it and see for yourself!</p>';
+		// Radio buttons for button text options
+		$button_labels = array("Sign Up Soon!", "Sign Up Now!", "Signup Closed");
+
+		echo '<p><label for="' . $this->get_field_id('button_text') . '">Button Text:</label><br />';
+		// Create radio buttons in a loop, for DRYness and to handle the 'checked' option better
+		foreach ($button_labels as $button_label) : 
+			$checked = "";
+
+			if ($button_label == $button_text) :
+				$checked = " checked";
+			endif;
+
+			echo '<input type="radio" id="' . $this->get_field_id('button_text') . '" name="'. $this->get_field_name('button_text') .'" value="' . $button_label . '"'. $checked .' /> ' . $button_label . '<br />';
+		endforeach;
 
 	}
 function update($new_instance, $old_instance) {
@@ -87,11 +99,7 @@ function widget($args, $instance) {
 		$subtitle = apply_filters( 'widget_text', $instance['subtitle'] );
 		$desc = apply_filters( 'widget_text', $instance['desc'] );
 		$page_to_link = apply_filters( 'widget_text', $instance['page_to_link'] );
-		$link_text = apply_filters( 'widget_text', $instance['link_text'] );
-
-		if ($link_text == "") :
-			$link_text = "Visit the ". get_the_title($page_to_link) ." page to learn more&nbsp;&raquo;";
-		endif;
+		$button_text = apply_filters( 'widget_text', $instance['button_text'] );
 
 		// before and after widget arguments are defined by themes
 		echo $args['before_widget'];
@@ -102,7 +110,17 @@ function widget($args, $instance) {
 		// This is where you run the code and display the output
 		echo '<div class="signup-section-subtitle">' . $subtitle . '</div>';
 		echo '<p class="signup-section-desc">' . $desc . '</p>';
-		echo '<a class="signup-section-button enabled" href="'. get_permalink( $page_to_link ) .'">'. $link_text .'</a>';
+
+		// Output button with proper class and href
+		$button_state = " disabled";
+		$button_href  = " javascript:void(0);";
+
+		if ($button_text == "Sign Up Now!") :
+			$button_state = " enabled";
+			$button_href  = get_permalink( $page_to_link );
+		endif;
+
+		echo '<a class="signup-section-button' . $button_state . '" href="'. $button_href .'">'. $button_text .'</a>';
 
 		echo $args['after_widget'];
 		
